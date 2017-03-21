72ab7106d83a52e228cb6d91018b3caa686d7594
Elin Abrahamsson <ekva@Elins-MacBook-Pro.local>
2017-03-21 13:01:17 +0100

    mm

---
+++ main.js
@@ -0,0 +1,20 @@
+let batman = document.querySelector("#batman");
+let america = document.querySelector("#america");
+consule.log("ready to rumble");
+
+
+batman.addEventListener("click",punch)
+
+function punch (){
+
+    consule.log("punch");
+    batman.classlist.add("headbutt");
+    batman.addEventListener("animationed")
+
+}
+
+
+function throwCaptainAmericaBack () {
+
+    console.log("throw cp")
+}
